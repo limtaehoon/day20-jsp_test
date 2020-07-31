47110ec3613e0b68bce9edb4971d3d7682272d03
th <bsee3313@gmail.com>
2020-07-31 16:40:35 +0900

    day20 jsp_test

---
+++ src/com/exam/Studentb.java
@@ -0,0 +1,42 @@
+package com.exam;
+
+public class Studentb{
+	private String name;
+	private String num;
+	private String gender;
+	private String major;
+	private String[] hobby;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getNum() {
+		return num;
+	}
+	public void setNum(String num) {
+		this.num = num;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getMajor() {
+		return major;
+	}
+	public void setMajor(String major) {
+		this.major = major;
+	}
+	public String[] getHobby() {
+		return hobby;
+	}
+	public void setHobby(String[] hobby) {
+		this.hobby = hobby;
+	}
+	
+	
+}
